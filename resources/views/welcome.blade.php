5b2359d932a82aa686d38c59322516ba381c9af4
Mattia Iusco <mattiaiusco@gmail.com>
2023-09-12 11:53:25 +0200

    installazione fortify completata e font importate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <x-layout title="rapidshop homepage">
 
-    <h1>ciso</h1>
+    <h1 class="tx-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis dolorum, repellendus ea, temporibus cumque, error ducimus adipisci veniam consequuntur ullam voluptate tenetur dolorem velit dolores natus iusto? Architecto, rem!</h1>
+
+    <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore soluta doloribus explicabo at reiciendis, reprehenderit facere magni dolores voluptatum assumenda, impedit expedita, mollitia voluptates nihil quaerat tenetur et eligendi vero?</p>
 
 </x-layout>
