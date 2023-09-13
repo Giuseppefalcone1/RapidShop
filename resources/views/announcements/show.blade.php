fb92cee9e5704661feda2e912f45721ca0289e80
Mattia Iusco <mattiaiusco@gmail.com>
2023-09-13 12:15:24 +0200

    Pagina show implementata

---
+++ resources/views/announcements/show.blade.php
@@ -0,0 +1,53 @@
+<x-layout>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <div class="card rounded-4">
+                    <!-- inizio carosello -->
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner p-2">
+                            <div class="carousel-item active">
+                                <img src="https://picsum.photos/300" class="d-block w-100 rounded-4 img-fluid p-2" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/300" class="d-block w-100 rounded-4 img-fluid p-2" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/300" class="d-block w-100 rounded-4 img-fluid p-2" alt="">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <!-- fine carosello -->
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $announcement->name }}</h5>
+                        <p class="card-text">{{ $announcement->description }}</p>
+                        <div class="row">
+                            <div class="col-6">
+                                <p>{{ $announcement->price }}</p>
+                            </div>
+                        </div>
+                        <a href="#" class="nav-link">{{ $announcement->category->name }}</a>
+                        <div class="card-footer">
+                            <p>data di pubblicazione: {{ $announcement->created_at->format('d/m/y') }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
--- resources/views/components/card-show.blade.php
+++ resources/views/components/card-show.blade.php
@@ -7,9 +7,6 @@
             <div class="col-6">
                 <p>{{ $announcement->price }}</p>
             </div>
-            <div class="col-6">
-                <a href="{{ route('announcement.show', $announcement) }}" class="btn btn-primary">Dettagli</a>
-            </div>
         </div>
         <a href="#" class="nav-link">{{ $announcement->category->name }}</a>
         <div class="card-footer">
