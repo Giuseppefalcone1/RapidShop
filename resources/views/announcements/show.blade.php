0b489930ca57f13ec285fd76bb177f91606c027c
Davidermellino <ermellinodavide@gmail.com>
2023-09-18 14:38:12 +0200

    correzione card detteglio

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
-
-<x-cardShow :announcement='$announcement' />
-
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-5 col-md-6 col-lg-5">
+            <x-cardShow :announcement='$announcement' />
+        </div>
+    </div>
 </x-layout>
