dae2f5cb2bf9f5597774152a917e698d2cf4c075
Giuseppe Falcone <giuseppe.falcone1@hotmail.com>
2023-09-22 12:17:24 +0200

    user story 8 in lavorazione

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Livewire\Component;
 use App\Models\Category;
 use App\Jobs\ResizeImage;
@@ -86,9 +87,13 @@ class CreateAnnouncement extends Component
                     'path' => $image->store($newFileName, 'public'),
                 ]);
 
-                dispatch(new ResizeImage($newImage->path , 300 , 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+
+                    new ResizeImage($newImage->path , 300 , 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
